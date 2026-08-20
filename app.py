bf66c8c4b71b5d62864eeb8aa0c4c7c14d40366e
Noctisdc <camijavier@hotmail.com>
2026-08-20 11:04:03 -0500

    Fix string formatting and update story text

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ except:
     pass
 
 st.subheader("El mono y el cangrejo.")
-st.write(" Un cangrejo encontró una bola de arroz, y un mono, envidioso, la cambió por una semilla de caqui que él mismo había hallado.
+st.write('Un cangrejo encontró una bola de arroz, y un mono, envidioso, la cambió por una semilla de caqui que él mismo había hallado.
 El cangrejo plantó la semilla, que creció hasta convertirse en un árbol cargado de frutos, pero no podía trepar para alcanzarlos.
 El mono se ofreció a subir por él, y una vez arriba se comió todos los caquis maduros; cuando el cangrejo le pidió su parte,
 le arrojó uno verde y duro que lo hirió de muerte.
 Sus hijos, ayudados por una castaña, una avispa y un mortero,
 prepararon una trampa: la castaña quemó al mono en el fogón,
 la avispa lo picó cuando fue a buscar agua,
-y el mortero cayó sobre él desde el techo. Así, el cangrejo fue vengado.")
+y el mortero cayó sobre él desde el techo. Así, el cangrejo fue vengado.').
            
 st.markdown(f"Quieres escucharlo?, copia el texto")
 text = st.text_area("Ingrese El texto a escuchar.")
